41e4a74ebc905581031b410e56802711a9da4aa8
carl804 <carl@hotprospector.com>
2025-10-09 17:18:15 +0000

    test: try all tab for conversations

--- src/components/tickets/TicketDetailSheet.tsx
+++ src/components/tickets/TicketDetailSheet.tsx
@@ -144,7 +144,7 @@ function TicketDetailSheet({ ticket, open, onOpenChange }: TicketDetailSheetProp
         return;
       }
       
-      const ghlUrl = `https://app.gohighlevel.com/v2/location/${locationId}/conversations/conversations/${conversationId}`;
+      const ghlUrl = `https://app.gohighlevel.com/v2/location/${locationId}/conversations/all/${conversationId}`;
       window.open(ghlUrl, "_blank");
     } catch (error) {
       toast.error("Failed to fetch conversation");
